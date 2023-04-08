a7ba693ad35c9019d11fd189eedabe8c657dbda8
YevhenTarashchuk <yevhen.tarashchuk@coreteka.com>
2023-04-08 13:26:00 +0300

    update routing

--- src/main/java/com/sacret/note/constant/AuthConstants.java
+++ src/main/java/com/sacret/note/constant/AuthConstants.java
@@ -6,7 +6,7 @@ public class AuthConstants {
     public static final String HEADER_PARAM_JWT_TOKEN = "Authorization";
     public static final String TOKEN_TYPE_CLAIM = "tokenType";
     public static final String ROLE_ATTRIBUTE = "role";
-    public static final String POST_PATH = "/v1/user/posts";
+    public static final String POST_PATH = "/v1/posts";
 
     private AuthConstants() {
 
